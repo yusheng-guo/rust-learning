ab3cf9ecb150a7d238879b533c3781456691a5ea
unknown <yushengguo557@gmail.com>
2023-05-16 23:34:21 +0800

    basic syntax

---
+++ basic_syntax/src/main.rs
@@ -0,0 +1,39 @@
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+// 变量和可变性
+// fn main() {
+    // let x = 5;
+    // let mut x = 5;
+    // println!("The value of x is: {x}");
+    // x = 6;
+    // println!("The value of x is: {x}");
+
+    // 常量 
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    // println!("{THREE_HOURS_IN_SECONDS}");
+
+    // 隐藏
+    // 
+    // let x = 5;
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in the inner scope is: {x}");
+    // }
+    // println!("The value of x is: {x}");
+
+    //
+    // let spaces = "   ";
+    // let spaces = spaces.len();
+// }
+
+// 数据类型
+fn main(){
+    // 类型注解
+    // let guess: u32 = "42".parse().expect("Not a number!");
+
+    // 标量 => 一个单独的值
+    // 整型 浮点型 布尔类型 字符类型
+}
